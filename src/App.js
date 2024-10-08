3526db1d506599b3f4cd652e419eb52f188c41e5
Aadarsh Singh <aadarsh4492@gmail.com>
2024-10-08 11:17:35 +0530

    Food Delivery App Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Home from './components/HomeComponent';
+import About from './components/AboutComponent';
+import Contact from './components/ContactComponent';
+import Menu from './components/MenuComponent';
+import Header from './components/HeaderComponent';
+import Footer from './components/FooterComponent';
+import { DISHES } from './shared/dishes'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
+  //let [dishes, setDishes] = useState(DISHES);
+  let dishes = DISHES;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route exact path="/menu" element={<Menu dishes={dishes} />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+
   );
 }
-
-export default App;
